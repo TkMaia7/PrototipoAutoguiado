f5c262a993b471698d446fe4997180cb03af5492
pame11a <pmellaf29@gmail.com>
2025-12-02 14:34:01 -0300

    Implementação do Mapa

--- App.js
+++ App.js
@@ -2,13 +2,30 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { Header } from './src/components/header';
 import { styles } from './styles';
+import Mapbox from '@rnmapbox/maps';
+Mapbox.setAccessToken("pk.eyJ1IjoicGFtZTExYSIsImEiOiJjbWlnMjY5c2IwMmRsM2Zwb3FzamxkbGg3In0.335QLoA_FqHQDF6EGP49EQ")
 
-export default function App() {
-  return (
+export default function TuorMap() {
+  const LocalizacaoInicial={
+    centerCoordinate: [-46.6333, -23.5505], //COORDENADA DO IFNMG
+    zoomLevel: 12,
+  };
+
+  return(
     <View style={styles.container}>
-      <Header/>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Mapbox.MapView 
+        style={styles.map}
+        styleURL={Mapbox.StyleURL.Street}
+      >
+        <Mapbox.Camera
+          centerCoordinate={LocalizacaoInicial.centerCoordinate}
+          animationMode="Flyto"
+        />
+        <Mapbox.PointAnnotation
+          id="meuPontoInicial"
+          coordinate={[-46.6333, -23.5505]}
+        />
+      </Mapbox.MapView>
     </View>
   );
 }
